b96e037b1a44dcf3cf1513bd5a260c911c94c3c2
kenosama <pc.tcanomorant@me.com>
2023-04-13 17:01:25 +0200

    🎨 style(properties/form.blade.php, shared/input.blade.php): improve form layout and add textarea support This commit improves the layout of the property form by adding a row and column structure to the form inputs. It also adds support for textarea inputs in the shared input component. The label for the input is now automatically generated from the name of the input if it is not provided.

--- resources/views/admin/properties/form.blade.php
+++ resources/views/admin/properties/form.blade.php
@@ -4,11 +4,22 @@
 @section('content')
     
 <h1>@yield('title')</h1>
-<form action="{{route($property->exist ? 'admin.property.update ' : 'admin.property.store', $property) }}" method="POST">
+<form class="vstack gap-2" action="{{route($property->exist ? 'admin.property.update ' : 'admin.property.store', $property) }}" method="POST">
 @csrf
 @method($property->exist ? 'put': 'post')
 
-@include('shared.input', ['label'=>'Title', 'name'=>'title', 'value'=>$property->title])
+    <div class="row">
+        @include('shared.input', ['class'=>'col', 'label'=>'Title', 'name'=>'title', 'value'=>$property->title])
+        <div class="col row">
+            @include('shared.input', ['class'=>'col', 'name'=>'surface', 'value'=>$property->surface])
+            @include('shared.input', ['class'=>'col', 'name'=>'price', 'value'=>$property->price])
+        </div>
+    </div>
+@include('shared.input', ['type'=>'textarea', 'name'=>'description', 'value'=>$property->description])
+<div class="row">
+    @include('shared.input', ['class'=>'col', 'name'=>'rooms', 'value'=>$property->rooms])
+</div>
+
 <div>
     <button class="btn btn-primary">
         @if($property->exist)
--- resources/views/shared/input.blade.php
+++ resources/views/shared/input.blade.php
@@ -1,19 +1,23 @@
 @php
-    $label ??= null;
     $type ??= 'text';
     $class ??= null;
     $name ??= '';
     $value ??= '';
+    $label ??= ucfirst($name);
 @endphp
 
 <div @class(["form-group", $class, "mb-3"])>
     <label for="{{$name}}">
         {{$label}}
     </label>
+    @if ($type === 'textarea')
+        <textarea class="form-control @error($name) is-invalid @enderror"  name="{{$name}}">{{ old($name, $value)}}</textarea>
+    @else
     <input class="form-control @error($name) is-invalid @enderror" type="{{$type}}" name="{{$name}}" value=" {{ old($name, $value)}}">
     @error($name)
     <div class="invalid-feedback">
         {{$message }}
     </div>
     @enderror
+    @endif
 </div>

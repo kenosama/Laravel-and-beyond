3af7b5688fa4a1b83acb03c69bfab3093600cd0f
kenosama <pc.tcanomorant@me.com>
2023-04-14 14:21:12 +0200

    🔨 refactor(OptionController.php): use OptionFormRequest instead of Request in store and update methods ✨ feat(OptionController.php): add success messages to store, update, and delete methods The OptionController.php file has been refactored to use the OptionFormRequest instead of the Request class in the store and update methods. This change improves the readability of the code and ensures that the validation rules are in one place. Additionally, success messages have been added to the store, update, and delete methods to provide feedback to the user when an option has been created, modified, or deleted.

--- app/Http/Controllers/Admin/OptionController.php
+++ app/Http/Controllers/Admin/OptionController.php
@@ -3,16 +3,17 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Http\Requests\Admin\OptionFormRequest;
+use App\Models\Option;
 
 class OptionController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
+
     public function index()
     {
-        //
+        return view('admin.options.index', [
+            'options' => Option::paginate(25)
+        ]);
     }
 
     /**
@@ -20,46 +21,46 @@ class OptionController extends Controller
      */
     public function create()
     {
-        //
+        $option = new Option();
+        return view('admin.options.form', [
+            'option' => $option
+        ]);
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(OptionFormRequest $request)
     {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
+        $option = Option::create($request->validated());
+        return to_route('admin.option.index')->with('success', 'Option has been Created');
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(Option $option)
     {
-        //
+        return view('admin.options.form', [
+            'option' => $option
+        ]);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(OptionFormRequest $request, Option $option)
     {
-        //
+        $option->update($request->validated());
+        return to_route('admin.option.index')->with('success', 'Option has been Modified');
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Option $option)
     {
-        //
+        $option->delete();
+        return to_route('admin.option.index')->with('success', 'Option has been Deleted');
     }
 }

17eb11281a1f442c2379c789d679267e15d67362
kenosama <pc.tcanomorant@me.com>
2023-04-14 14:47:42 +0200

    🎨 style(admin.blade.php): add error message display This commit adds an error message display to the admin view. If there are any errors, they will be displayed in a red alert box. This improves the user experience by providing feedback on any errors that may have occurred.

--- resources/views/admin/admin.blade.php
+++ resources/views/admin/admin.blade.php
@@ -16,7 +16,16 @@
         </div>
             
         @endif
-
+        
+        @if ($errors->any())
+            <div class="alert alert-danger">
+                <ul class='my-0'>
+                    @foreach ($errors->all() as $error)
+                        <li>{{$error}}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
         @yield('content')
     </div>
 </body>

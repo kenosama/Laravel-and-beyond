ab5c2a0de88183a355db8c03fc9a6ffd5974be10
kenosama <pc.tcanomorant@me.com>
2023-04-14 16:33:07 +0200

    🐛 fix(select.blade.php): fix selected option in select dropdown The selected option in the select dropdown was not being set correctly. The fix updates the blade template to use the correct variable names and syntax to set the selected option.

--- resources/views/shared/select.blade.php
+++ resources/views/shared/select.blade.php
@@ -8,8 +8,8 @@ $label ??= ucfirst($name);
     <label for="{{ $name }}">{{ $label }}</label>
     
     <select name="{{$name}}[]" id="{{$name}}" multiple>
-        @foreach ($options as $key =>$value)
-            <option value="{{$key}}">{{$value}}</option>
+        @foreach($options as $k => $v)
+            <option @selected($value->contains($k)) value="{{ $k }}">{{ $v }}</option>
         @endforeach
     </select>
 

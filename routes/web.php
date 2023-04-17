8832100b7ef5760820c1a2f67d288e7d9b811dfe
kenosama <pc.tcanomorant@me.com>
2023-04-17 11:21:25 +0200

    🚀 feat(PropertyController.php, web.php): add PropertyController and routes for property index and show The PropertyController has been added with two methods, index and show. The index method is used to display a list of properties, while the show method is used to display a single property. The web.php file has been updated to include routes for the property index and show pages. The show route includes a slug and property ID in the URL, which is validated using regular expressions.

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,15 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+$idRegex='[0-9]+';
+$slugRegex='[0-9a-Z\-]+';
 
 Route::get('/', [HomeController::class, 'index']);
+Route::get('/Properties', [PropertyController::class, 'index'])->name('property.index');
+Route::get('/Properties/{slug}-{property}', [PropertyController::class, 'show'])->name('property.show')->where([
+    'property'=>$idRegex,
+    'slug'=>$slugRegex
+]);
 
 Route::prefix('admin')->name('admin.')->group(function(){
     Route::resource('property', PropertyController::class)->except(['show']);

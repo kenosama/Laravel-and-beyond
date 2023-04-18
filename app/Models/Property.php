5347f8c2bba4e79325750544e020c723e9838f17
kenosama <pc.tcanomorant@me.com>
2023-04-18 16:41:48 +0200

    🔥 refactor(PropertyController.php): add withTrashed() to properties query 🔒 chore(Property.php): add SoftDeletes trait to Property model 🗃️ chore(2023_04_18_143618_add_deleted_at_to_properties.php): add migration to add deleted_at column to properties table The `withTrashed()` method is added to the properties query in the `index()` method of the `PropertyController` to include soft-deleted properties in the results. The `SoftDeletes` trait is added to the `Property` model to enable soft-deleting of properties. A migration is created to add a `deleted_at` column to the `properties` table, which is used by the `SoftDeletes` trait to mark properties as deleted instead of actually deleting them from the database.

--- app/Models/Property.php
+++ app/Models/Property.php
@@ -7,12 +7,14 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Str;
 
 class Property extends Model
 {
     use HasFactory;
+    use SoftDeletes;
 
     protected $fillable = [
         'title',
